896cb9fe287e10b539b4b4504b81cb5fea2d17c6
longtv <longtv@oneempower.com>
2024-08-20 17:21:32 +0700

    #init project

---
+++ longtv/src/main/java/com/example/longtv/longtv/LongtvApplication.java
@@ -0,0 +1,13 @@
+package com.example.longtv.longtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongtvApplication.class, args);
+	}
+
+}
---
+++ longtv/src/test/java/com/example/longtv/longtv/LongtvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.longtv.longtv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
